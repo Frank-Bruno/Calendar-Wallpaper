f82af50657fb2178e514b901729fa47ca30b1052
João <joaodacruznatividade@gmail.com>
2021-08-11 12:25:15 -0300

    mudanças

--- src/project/Principal.java
+++ src/project/Principal.java
@@ -1,24 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//comente
 package project;
 
-
-
-/**
- *
- * @author joaod
- */
+// Classe principal do projeto
 public class Principal{
     
     public static void main(String args[]) {
+       //instancia a classe resolução
        resolucao r = new resolucao();
+       //instancia a classe Semana
        Semana Semana = new Semana();
+       /* usando o comando getLar é usado a largura da tela que a classe semana calculou 
+       menos a largura do jframe da classe semana*/
        int x = r.getLar() - Semana.getWidth();
        int y = 1;
+       // seta a localização do jframe da classe semana
        Semana.setLocation(x, y);
+       // torna o jframe da classe semana visivel
        Semana.setVisible(true);
  
          
